0e7e89f485c2c4ceef81ce5f455b21e6208fe21a
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-03 19:58:04 +0530

    Added:Welcome message

---
+++ src/com/blz/addressBook/AddressBook.java
@@ -0,0 +1,8 @@
+package com.blz.addressBook;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+            System.out.println("Welcome to Address Book program");
+    }
+}
