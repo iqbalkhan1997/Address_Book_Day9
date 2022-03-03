9b00b1d2524511cd3de1d314e61d633201a4b737
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-03 21:25:27 +0530

    Added:contacts in Address book

--- src/com/blz/addressBook/AddressBook.java
+++ src/com/blz/addressBook/AddressBook.java
@@ -1,6 +1,13 @@
 package com.blz.addressBook;
 
 public class AddressBook {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private int zip;
+    private int phoneNumber;
+    private String email;
 
     public static void main(String[] args) {
             System.out.println("Welcome to Address Book program");
